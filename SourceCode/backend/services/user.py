5ec9ff47827137c789a502501b79e335388d9609
Duy Anh Nguyen <AnhND.B22CN025@stu.ptit.edu.vn>
2025-12-05 22:42:27 +0700

    fix

--- SourceCode/backend/services/user.py
+++ SourceCode/backend/services/user.py
@@ -1,6 +1,10 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+from starlette.responses import JSONResponse
 
 from configs.authentication import hash_password
+from exceptions.exception_handler import raise_error
 from models import User
 from schemas.base_response import BaseResponse
 from schemas.user import UserCreate
@@ -14,7 +18,16 @@ def get_user_service():
 
 
 class UserService:
-    async def register(self, data: UserCreate, db: AsyncSession) -> BaseResponse:
+    async def register(self, data: UserCreate, db: AsyncSession) -> BaseResponse | JSONResponse:
+        existing_user = await db.execute(
+            select(User).where(User.username == data.username)
+        )
+        if existing_user.scalar_one_or_none() is not None:
+            return raise_error(
+                status_code=status.HTTP_409_CONFLICT,
+                message='Username already exists'
+            )
+
         new_user = User(
             username=data.username,
             hashed_password=hash_password(data.password),
