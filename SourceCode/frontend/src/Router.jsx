98a05371dbb160221791f4c6d7778e1fd59e3baf
Duy Anh Nguyen <AnhND.B22CN025@stu.ptit.edu.vn>
2025-12-08 18:33:34 +0700

    Integrate Register and Login Page

--- SourceCode/frontend/src/Router.jsx
+++ SourceCode/frontend/src/Router.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import UpdateOTA from './pages/UpdateOTA';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProtectedRoute from './components/common/ProtectedRoute';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
-      <Route path="/dashboard" element={<Home />} />
-      <Route path="/ota" element={<UpdateOTA />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/ota"
+        element={
+          <ProtectedRoute>
+            <UpdateOTA />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
