f29ff740b3603cc0f9411af9bd82d49af626eed0
buiduc212004 <buiduc210102004@gmail.com>
2025-12-08 18:29:58 +0700

    fix

--- SourceCode/backend/main.py
+++ SourceCode/backend/main.py
@@ -3,8 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from configs.database import Base, async_engine
 from models import *
-from routers import authentication, user, face
-from services.mqtt_client import start_mqtt, stop_mqtt
+from routers import authentication, user
 
 app = FastAPI()
 
@@ -13,18 +12,15 @@ app = FastAPI()
 async def on_startup():
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    start_mqtt()
 
 
 @app.on_event('shutdown')
 async def on_shutdown():
     await async_engine.dispose()
-    stop_mqtt()
 
 
 app.include_router(authentication.router)
 app.include_router(user.router)
-app.include_router(face.router)
 
 app.add_middleware(
     CORSMiddleware,
